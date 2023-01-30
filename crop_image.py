c81bca1036f18a020f397948312e513bc72420e4
ThornGote <cj.roshi@gmail.com>
2023-01-30 15:17:25 -0500

    Basic GUI setup, edge detection

--- crop_image.py
+++ crop_image.py
@@ -2,28 +2,56 @@ import numpy as np
 import cv2
 import time
 
+def detect_edges(image):
+    # convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # apply GaussianBlur to reduce noise
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    
+    # apply Canny edge detection
+    canny = cv2.Canny(blurred, 50, 150)
+    
+    return canny
+
 # init camera (1 refers to the rear cam)
 cam = cv2.VideoCapture(1)
 
 # allow shutter to open/autofocus
-time.sleep(1)
+time.sleep(3)
 
 result, image = cam.read()
 
-print(image)
-if result:
+# Show image in window
+cv2.imshow("frame", image)
+
+
+while result:
+    # Capture the video frame
+    # by frame
+    ret, frame = cam.read()
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(81)  == ord('q'):
+        break
+
+    if cv2.waitKey(13) == ord('\r'):
+        print(frame)
+
+        # Detect edges in the image
+        edges = detect_edges(frame)
+
+        # Display the edges
+        cv2.imshow('Edges', edges)
+
+        
   
-    # Show image in window
-    cv2.imshow("CALIBR8", image)
-  
-    # Save locally
-    cv2.imwrite("CALIBR8.png", image)
-  
-    # If keyboard interrupt occurs, destroy image 
-    # window
-    cv2.waitKey(0)
-    cv2.destroyWindow("CALIBR8")
-  
-# If captured image is corrupted/no camera exists
-else:
-    print("No image detected. Please try again.")
+# After the loop release the cap object
+cam.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
