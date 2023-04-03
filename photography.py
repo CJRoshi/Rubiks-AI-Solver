5b7d4984138d74c2b50b5b8ed868c1775ce46617
ThornGote <cj.roshi@gmail.com>
2023-04-03 15:31:20 -0400

    Fixed Photography to standardize our stuff!!

--- photography.py
+++ photography.py
@@ -9,7 +9,10 @@ result, img = cam.read()
 
 # Time delay to allow the shutter to autofocus.
 time.sleep(0.5)
-image_num=48
+image_num=75
+
+folder = "dataset_imgs/"
+
 while result:
     # Capture video frame by frame
     result, img = cam.read()
@@ -26,8 +29,42 @@ while result:
 
     # Use P to capture and process images.
     if cv2.waitKey(80) == ord('p'):
-        cv2.imwrite('rubiks_img_'+str(image_num)+'.png', img)
+
+        ### IMAGE PROCESSING ###
+
+        imgheight, imgwidth, depth = np.shape(img)
+
+        if imgwidth >= imgheight:
+            larger_side = imgwidth
+            smaller_side = imgheight
+
+            border_top  = (smaller_side)//8
+            border_side = (larger_side)//2-3*border_top
+            true_center_width = 4*border_top
+            center_width = 6*border_top
+
+        elif imgwidth < imgheight:
+            larger_side = imgheight
+            smaller_side = imgwidth
+
+            border_side  = (smaller_side)//8
+            border_top = (larger_side)//2-3*border_side
+            true_center_width = 4*border_side
+            center_width = 6*border_side
+
+        # Crop the img to the above dimensions.
+        img_cropped = np.copy(img)
+        img_cropped = img_cropped[border_top:border_top+center_width, border_side:border_side+center_width]
+        
+        ### FILE CLASSIFICATION ###
+        edge = "edge_" if "y" in input("Is this an edged cube? (Y/N)\n").lower() else "noedge_"
+        print("Write the colors on this face in sequence, such as 'WWWWWWWWW' for all-white or 'ROYGBWBGY'.")
+        print("R is RED\nO is ORANGE\nY is YELLOW\nG is GREEN\nB is BLUE\nW is WHITE\n")
+        colstring = input().upper() + "_"
+        small = "large_" if "y" in input("Is this a closeup? (Y/N)\n").lower() else "small_"
+        cv2.imwrite((folder+edge+colstring+small+str(image_num)+'.png'), img)
         image_num+=1
+        
         cv2.destroyAllWindows()
         time.sleep(0.75)
 
--- standardize.py
+++ standardize.py
@@ -8,28 +8,29 @@ for filename in os.listdir(file_folder):
     true_filename = file_folder+"/"+filename
     img = cv2.imread(true_filename)
 
-    imgheight, imgwidth, depth = np.shape(img)
+    if  "_" not in true_filename[-6:-4] and int(true_filename[-6:-4]) > 60:
+        imgheight, imgwidth, depth = np.shape(img)
 
-    if imgwidth >= imgheight:
-        larger_side = imgwidth
-        smaller_side = imgheight
+        if imgwidth >= imgheight:
+            larger_side = imgwidth
+            smaller_side = imgheight
 
-        border_top  = (smaller_side)//8
-        border_side = (larger_side)//2-3*border_top
-        true_center_width = 4*border_top
-        center_width = 6*border_top
+            border_top  = (smaller_side)//8
+            border_side = (larger_side)//2-3*border_top
+            true_center_width = 4*border_top
+            center_width = 6*border_top
 
-    elif imgwidth < imgheight:
-        larger_side = imgheight
-        smaller_side = imgwidth
+        elif imgwidth < imgheight:
+            larger_side = imgheight
+            smaller_side = imgwidth
 
-        border_side  = (smaller_side)//8
-        border_top = (larger_side)//2-3*border_side
-        true_center_width = 4*border_side
-        center_width = 6*border_side
+            border_side  = (smaller_side)//8
+            border_top = (larger_side)//2-3*border_side
+            true_center_width = 4*border_side
+            center_width = 6*border_side
 
-    # Crop the img to the above dimensions.
-    img_cropped = np.copy(img)
-    img_cropped = img_cropped[border_top:border_top+center_width, border_side:border_side+center_width]
+        # Crop the img to the above dimensions.
+        img_cropped = np.copy(img)
+        img_cropped = img_cropped[border_top:border_top+center_width, border_side:border_side+center_width]
 
-    cv2.imwrite(true_filename, img_cropped)
+        cv2.imwrite(true_filename, img_cropped)
